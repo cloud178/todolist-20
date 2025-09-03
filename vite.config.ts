58274feb5a4827146e1a3234aea85a5abf853bea
Denis Biryukov <denis-biryukov-13@mail.ru>
2025-09-03 21:13:02 +0300

    feat: gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react-swc"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "todolist-20",
   resolve: {
     alias: {
       "@/": `${path.resolve(__dirname, "src")}/`,
